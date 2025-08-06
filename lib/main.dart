abf08cf8dd3c75751abacd8d6c5ce396ef2b4321
malifadali-gif <malifadali@gmail.com>
2025-08-06 09:30:19 +0300

    third  commit

--- lib/main.dart
+++ lib/main.dart
@@ -67,6 +67,9 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       // called again, and so nothing would appear to happen.
+
+
+
       _counter++;
     });
   }
