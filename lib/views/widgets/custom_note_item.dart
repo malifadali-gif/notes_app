ac9db59aee8c1adbff5f63dc924e1d3401904cb1
malifadali-gif <malifadali@gmail.com>
2025-08-07 11:10:37 +0300

    create noteListview

---
+++ lib/views/widgets/custom_note_item.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class NotesItem extends StatelessWidget {
+  const NotesItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(top: 24 , bottom: 20 ,left: 16 , ),
+      decoration: BoxDecoration(
+          color: Colors.amber,
+          borderRadius: BorderRadius.circular(16)
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text('Flutter Tips' , style: TextStyle(color: Colors.black , fontSize: 26),),
+            subtitle: Text('Build your career with fadaly' ,
+              style: TextStyle(color: Colors.black38 ,
+                  fontSize: 16),),
+            trailing: IconButton(onPressed: (){}, icon: Icon(Icons.delete , size: 24,) , color: Colors.black,),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Text('May 21 , 2022' ,style: TextStyle(color: Colors.black38),),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/views/widgets/notes_view_body.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_app_bar.dart';
+
+import 'custom_note_item.dart';
+import 'notes_list_view.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16 ),
+      child: Column(
+        children: const [
+          SizedBox(height: 60,),
+          CustomAppBar(),
+          Expanded(child: NotesListView())
+        ],
+      ),
+    );
+  }
+}
+
