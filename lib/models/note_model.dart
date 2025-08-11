9b554f75e44aa7585ced49748144d108cc54a7e5
malifadali-gif <malifadali@gmail.com>
2025-08-11 12:19:39 +0300

    create note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,10 @@
+class noteModel
+{
+final String title;
+final String subTitle;
+final String date;
+final int color;
+
+  noteModel({required this.title, required this.subTitle, required this.date, required this.color});
+
+}
