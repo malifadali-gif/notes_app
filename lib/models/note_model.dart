b0dcc59511afbf01ccc7c18bcc7e23f9ab5ef1eb
malifadali-gif <malifadali@gmail.com>
2025-08-11 14:12:48 +0300

    creat type adapter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,15 @@
-class noteModel
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+@HiveType(typeId: 0)
+class noteModel extends HiveObject
 {
+  @HiveField(0)
 final String title;
+  @HiveField(1)
 final String subTitle;
+  @HiveField(2)
 final String date;
+  @HiveField(3)
 final int color;
 
   noteModel({required this.title, required this.subTitle, required this.date, required this.color});
