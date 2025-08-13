f948f80bdb91521f183183b9f1582fc35d021132
malifadali-gif <malifadali@gmail.com>
2025-08-13 13:19:58 +0300

    add add note cubit to add note bottom sheet

--- lib/views/widgets/add_bottom_sheet.dart
+++ lib/views/widgets/add_bottom_sheet.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/constants.dart';
+import 'package:notes_app/cubits/add_note_cubit.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
 
+import 'add_note_form.dart';
 import 'custom_buttom.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -9,69 +13,28 @@ class AddNoteButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
+    return  Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: SingleChildScrollView(
-        child:  AddNoteForm()
+            child: BlocConsumer<AddNoteCubit, AddNoteState>(
+              listener: (context, state) {
+                if(state is AddNoteFailure)
+                {
+                  print('failed ${state.errMessage}');
+                }
+                if(state is AddNoteSucces)
+                {
+                  Navigator.pop(context);
+                }
+
+              },
+              builder: (context, state) {
+                return ModalProgressHUD(
+                    inAsyncCall: state is AddNoteLoading ? true : false ,
+                    child: const AddNoteForm());
+              },
+            )
         )
     );
   }
 }
-
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
-
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-
-  final GlobalKey<FormState> formKey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-  String? title , subTitle;
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      autovalidateMode: autovalidateMode,
-      child:  Column(
-        children: [
-         const SizedBox(height: 32,),
-         CustomTextField(
-           onSaved: (value)
-           {
-             title = value;
-           },
-           hint: 'Title',),
-          const SizedBox(height: 16,) ,
-          CustomTextField(
-            onSaved: (value)
-            {
-              subTitle = value;
-            },
-            hint: 'Content', maxlines: 5,),
-         const SizedBox(height: 80,),
-          CustomButtom(
-            onTap: ()
-            {
-              if(formKey.currentState!.validate())
-              {
-                formKey.currentState!.save();
-              }else{
-                autovalidateMode =AutovalidateMode.always;
-                setState(() {
-
-                });
-              }
-            },
-          ),
-          SizedBox(height: 60,)
-        ],
-      ),
-    );
-  }
-}
-
